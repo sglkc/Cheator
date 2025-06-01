be96797b897a7df39620fa1ced12c96cade5dbae
Seya <31957516+sglkc@users.noreply.github.com>
2025-06-01 17:42:54 +0700

    hapus python semuanya

--- accounts/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    nim = forms.CharField(label='NIM', max_length=50)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
